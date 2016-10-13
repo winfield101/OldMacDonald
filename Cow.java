f5c2d7f2721b05b7347b16799753239ef4f126af
winfield101 <winfield703@gmail.com>
2016-10-13 10:30:37 -0700

    a

--- Cow.java
+++ Cow.java
@@ -1,4 +1,23 @@
-class Cow //implements Animal 
+class Cow implements Animal 
 {     
-     //your code here
+   	private String myType;     
+    private String mySound;      
+    public Cow(String type, String sound)     
+    {         
+         myType = type;         
+         mySound = sound;     
+    }     
+     public Cow()     {         
+         myType = "cow";         
+         mySound = "moo";     
+    }      
+     public String getSound()
+    {
+     	return mySound;
+    }     
+     public String getType()
+    {
+     	return myType;
+    } 
 }
+
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,6 @@
 
 void setup()
 {
-	//your variable declarations here
+	
 }
 
