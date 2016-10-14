20a77dbf93d1600177e58eb9a357f4e9e75174ac
winfield101 <winfield703@gmail.com>
2016-10-14 10:46:11 -0700

    a

--- Chick.java
+++ Chick.java
@@ -1,4 +1,14 @@
-class Chick implements Animal 
-{     
-     //your code here
+class Chick implements Animal {     
+     private String myType;     
+     private String mySound;      
+     public Chick(String type, String sound)     {         
+         myType = type;         
+         mySound = sound;     
+     }     
+     public Chick()     {         
+         myType = "chick";         
+         mySound = "cluck";     
+     }      
+     public String getSound(){return mySound;}     
+     public String getType(){return myType;} 
 }
